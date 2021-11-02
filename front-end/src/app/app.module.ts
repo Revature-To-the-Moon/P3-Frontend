afd9ac3a93389ddbe4d4f8bf0203181b0054bc1e
Carlos A Martinez <Linguz2@gmail.com>
2021-11-02 14:15:13 -0400

    Created components for profile.

--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -4,10 +4,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { RecentActivityComponent } from './profile/recent-activity/recent-activity.component';
+import { ListOfFollowersComponent } from './profile/list-of-followers/list-of-followers.component';
+import { FollowedPostsComponent } from './profile/followed-posts/followed-posts.component';
+import { FollowButtonComponent } from './profile/follow-button/follow-button.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RecentActivityComponent,
+    ListOfFollowersComponent,
+    FollowedPostsComponent,
+    FollowButtonComponent
   ],
   imports: [
     BrowserModule,
