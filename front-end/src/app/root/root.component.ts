d7bb6d22715a995d24502e9a1aa62340f21120d6
Brian Martinez <brian.martinez1356@gmail.com>
2021-11-02 16:18:49 -0400

    root componnet and service

---
+++ front-end/src/app/root/root.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { RootServiceService } from '../root-service.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './root.component.html',
+  styleUrls: ['./root.component.css']
+})
+export class RootComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
