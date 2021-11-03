5b157621835ac2970e42fb1f2b904861d81c6845
Peiferinc <peiferjustin96@gmail.com>
2021-11-03 11:58:53 -0500

    more dummy data

--- front-end/src/app/service/profile.service.ts
+++ front-end/src/app/service/profile.service.ts
@@ -1,9 +1,19 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProfileService {
 
-  constructor() { }
+  url = 'http//localhost:3000';
+
+  constructor(private http: HttpClient) { }
+  
+  getAll(): Observable<any> {
+    return this.http.get(this.url + '_sort=id&order=desc')
+    .pipe();
+  }
 }
