51e456d35f1c1465749dcfdae377ba0efb5942fd
hraose98 <harose98@revature.net>
2021-11-03 11:15:44 -0400

    auth

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-
-const routes: Routes = [
-
-  {
-    path: '',
-    component: LoginComponent
-  },
-=======
 import { RootComponent } from './root/root.component';
 
 const routes: Routes = [
   {
     path: 'root',
-    component: RootComponent,
+    component: RootComponent
+  },
+
+  {
+    path: '',
+    component: LoginComponent
   }
->>>>>>> dede38d5b4455a51fee9793b23737df4240b46c8
 ];
 
 @NgModule({
--- front-end/src/app/app.module.ts
+++ front-end/src/app/app.module.ts
@@ -1,29 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
-
-// Import the module from the SDK
 import { AuthModule } from '@auth0/auth0-angular';
-=======
 import { NavBarComponent } from './nav-bar/nav-bar.component';
->>>>>>> dede38d5b4455a51fee9793b23737df4240b46c8
 import { RootComponent } from './root/root.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
-    RootComponent
-=======
     RootComponent,
     NavBarComponent,
->>>>>>> dede38d5b4455a51fee9793b23737df4240b46c8
   ],
   imports: [
     BrowserModule,
