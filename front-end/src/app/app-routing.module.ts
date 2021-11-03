d21a5e38471b0ff5a8b3434d895408c06c8c4dd4
berry-peachy <karla.rodriguez@revature.net>
2021-11-03 10:48:04 -0400

    merge fix

--- front-end/src/app/app-routing.module.ts
+++ front-end/src/app/app-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
 import { LoginComponent } from './login/login.component';
+import { RootComponent } from './root/root.component';
 
 const routes: Routes = [
 
@@ -9,15 +9,11 @@ const routes: Routes = [
     path: '',
     component: LoginComponent
   },
-=======
-import { RootComponent } from './root/root.component';
 
-const routes: Routes = [
   {
     path: 'root',
     component: RootComponent,
   }
->>>>>>> dede38d5b4455a51fee9793b23737df4240b46c8
 ];
 
 @NgModule({
