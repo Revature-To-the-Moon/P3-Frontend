66cd43eb074eb2d242d390f2ca2b22cbf7232020
Larry Moua <lkmoua@gmail.com>
2021-11-04 15:25:25 -0400

    removed params and router for code smells

--- front-end/src/app/login/login.component.spec.ts
+++ front-end/src/app/login/login.component.spec.ts
@@ -1,7 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
-import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
+import { ActivatedRoute, RouterModule } from '@angular/router';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AuthModule } from '@auth0/auth0-angular';
 
