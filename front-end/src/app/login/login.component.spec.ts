1968fed0071ad0c937e21736d9d7e4b9ef358c18
Larry Moua <lkmoua@gmail.com>
2021-11-04 15:21:27 -0400

    fixed code smells and bugs

--- front-end/src/app/login/login.component.spec.ts
+++ front-end/src/app/login/login.component.spec.ts
@@ -1,14 +1,9 @@
-import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { LoginComponent } from './login.component';
 import { ActivatedRoute, Params, Router, RouterModule } from '@angular/router';
-import { Injectable } from '@angular/core';
 import { RouterTestingModule } from '@angular/router/testing';
-import { AuthService } from '@auth0/auth0-angular';
 import { AuthModule } from '@auth0/auth0-angular';
-import { Subject } from 'rxjs';
-import { UserCreationService } from '../service/user-creation.service';
-import {By} from '@angular/platform-browser';
 
 
 
@@ -17,8 +12,6 @@ describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
   let activatedRoute: ActivatedRoute = new ActivatedRoute;
-  let service: AuthService;
-  let router: Router;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -39,9 +32,6 @@ describe('LoginComponent', () => {
     })
     .compileComponents();
 
-    service = TestBed.inject(AuthService);
-    router = TestBed.inject(Router);
-
   });
 
   beforeEach(() => {
@@ -55,46 +45,6 @@ describe('LoginComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
-
-  /*
-  it('should have one button with "Login" on the page', () => {
-    const linkDes = fixture.debugElement.queryAll(By.css('button'));
-    const nativeButton: HTMLButtonElement = linkDes[0].nativeElement;
-    expect(nativeButton.textContent).toBe('Login');
-  });
-  */
-
-  /*
-  it('should have one button with "Register" on the page', () => {
-    const linkDes = fixture.debugElement.queryAll(By.css('button'));
-    const nativeButton: HTMLButtonElement = linkDes[1].nativeElement;
-    expect(nativeButton.textContent).toBe('Register');
-  });
-*/
-
-  it('should send data on submit', async () => {
-    const AuthServiceSpy = jasmine.createSpyObj('AuthService',['Login']);
-
-    let fixture = TestBed.createComponent(LoginComponent)
-    let component: LoginComponent = fixture.componentInstance;
-    let element = fixture.nativeElement;
-
-    fixture.detectChanges();
-
-    // expect(element.querySelector('#userService-username').value).toEqual(expectedUsername);
-
-    // element.querySelector(component.Loginfunc).toHaveBeenCalled();
-
-    // spyOn(component, 'click').and.returnValue();
-    // let buttonElement = fixture.debugElement.query(By.css('table'));;
-    // spyOn(component, 'click');
-    // await buttonElement.triggerEventHandler('click');
-    // fixture.detectChanges();
-    // expect(component.Loginfunc).toHaveBeenCalled();
-
-
-  });
-
 
 });
 
