aae9b94bc962b681cfb9675947022778a20213bd
Larry Moua <lkmoua@gmail.com>
2021-11-04 15:03:33 -0400

    Update karma.conf.js

--- front-end/karma.conf.js
+++ front-end/karma.conf.js
@@ -25,7 +25,8 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: './',
+      dir: require('path').join(__dirname, './coverage/front-end'),
+      subdir: '.',
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
