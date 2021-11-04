551e20569c24369d0c843884a9491ece44f8242b
Peiferinc <peiferjustin96@gmail.com>
2021-11-04 11:40:30 -0500

    ngfor problems

--- front-end/src/app/profile/list-of-followers/list-of-followers.component.ts
+++ front-end/src/app/profile/list-of-followers/list-of-followers.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input  } from '@angular/core';
+import { Router, ActivatedRoute, ParamMap } from '@angular/router';
+import { User } from 'src/app/models/user';
+import { ProfileService } from 'src/app/service/profile.service';
 
 @Component({
   selector: 'app-list-of-followers',
@@ -6,10 +9,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-of-followers.component.css']
 })
 export class ListOfFollowersComponent implements OnInit {
+  @Input() id = 0;
+  @Input() list!: number[];
+  nameList!: string[];
+  
 
-  constructor() { }
+  constructor(private route: ActivatedRoute,public profileService: ProfileService) { }
 
-  ngOnInit(): void {
+  ngOnInit() {
+      this.profileService.getUserById(this.id).then((result: User) => {
+        this.list= result.followedUsers;
+        console.log("list id is now "+this.list);
+      });      
+  }
+getNameof(id: number): string{
+  let username = "";
+  this.profileService.getUserById(id).then((result: User) => {
+    console.log(result.name);
+    username =  result.name;})
+    return username;
   }
 
 }
