d5e1b30d9698de92d9fc44f767ed84d857e07a20
Larry Moua <lkmoua@gmail.com>
2021-11-04 14:00:54 -0400

    changed karma to chrome headless

--- front-end/karma.conf.js
+++ front-end/karma.conf.js
@@ -36,11 +36,11 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['Chrome'],
+    browsers: ['MyHeadlessChrome'],
     singleRun: false,
     customLaunchers: {
       MyHeadlessChrome: {
-        base: 'Chrome',
+        base: 'ChromeHeadless',
         flags: ['--disable-translate', '--disable-extensions', '--remote-debugging-port=9223']
       }
     },
