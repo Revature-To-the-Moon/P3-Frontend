baa2337ba33109851d44b9a5a012650ecaa2f167
berry-peachy <karla.rodriguez@revature.net>
2021-11-05 10:48:51 -0400

    buffer setup

--- front-end/src/app/buffer/buffer.component.ts
+++ front-end/src/app/buffer/buffer.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-buffer',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BufferComponent implements OnInit {
 
-  constructor() { }
+  constructor(private auth: AuthService) { }
 
   ngOnInit(): void {
   }
