1f144cb0253ee41bfa674d06d5ea61fd5e3e751f
berry-peachy <karla.rodriguez@revature.net>
2021-11-05 14:14:40 -0400

    buffer

--- front-end/src/app/buffer/buffer.component.ts
+++ front-end/src/app/buffer/buffer.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '@auth0/auth0-angular';
-import { UserCreationService } from '../service/user-creation.service';
+import { ProfileService } from '../service/profile.service';
 import { User } from '../models/user';
+
 
 @Component({
   selector: 'app-buffer',
@@ -9,30 +10,21 @@ import { User } from '../models/user';
   styleUrls: ['./buffer.component.css']
 })
 export class BufferComponent implements OnInit {
-    flag: boolean = false;
-
-  constructor(private auth: AuthService, private userService: UserCreationService) { }
     
-  userlist: User[] = [];
+
+  constructor(private auth: AuthService, private profileService: ProfileService) { }
+    
+  
     user: User = {
       username: ''
     };
 
   ngOnInit(): void {
-    // this.auth.user$.subscribe(profile =>
-      // {
-      //   this.userService.userlist().then(result => {
-      //     for (let i = 0; i < result.length; i++) {
-      //       if (result[i].username == profile.username) {
-      //         this.flag = true
-      //         this.auth.loginWithRedirect({appState: {target: '/userprofile'}});
-      //       }
-      //     }
-      //     if (this.flag !== true) {
-      //       this.auth.loginWithRedirect({appState: {target: '/user-creation'}});
-      //     }
-        //   })
-        // })
-      // }
-  }
+    this.auth.user$.subscribe(profile =>
+      {
+        this.user.username = profile.preferred_username;
+        this.auth.loginWithRedirect({appState: {target: '/root'}});
+      }
+    )}
 }
+          
