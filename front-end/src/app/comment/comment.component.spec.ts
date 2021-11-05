7b171cf93d33d702c42db8e25330071047d9faa8
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-11-05 11:18:50 -0400

    added some testing to comments section

--- front-end/src/app/comment/comment.component.spec.ts
+++ front-end/src/app/comment/comment.component.spec.ts
@@ -1,16 +1,28 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { RootServiceService } from '../service/root-service.service';
 
 import { CommentComponent } from './comment.component';
 
 describe('CommentComponent', () => {
   let component: CommentComponent;
   let fixture: ComponentFixture<CommentComponent>;
+  let service: RootServiceService;
+  let router: Router; 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ CommentComponent ]
+      declarations: [ CommentComponent ], 
+      imports: [FormsModule, RouterTestingModule, HttpClientTestingModule]
     })
     .compileComponents();
+
+    service = TestBed.inject(RootServiceService);
+    router = TestBed.inject(Router)
   });
 
   beforeEach(() => {
@@ -22,4 +34,14 @@ describe('CommentComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should call onSubmit when form is submitted', () =>{
+    spyOn(component, 'onSubmit').and.returnValue();
+
+    let form = fixture.debugElement.query(By.css('form'))
+
+    form.triggerEventHandler('ngSubmit', null);
+
+    expect(component.onSubmit).toHaveBeenCalled();
+  })
 });
