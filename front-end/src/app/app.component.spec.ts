ee9112a3ae17c015b0e835827e02c34a26e53bd6
Hung Phan <htp217@gmail.com>
2021-11-05 11:02:18 -0400

    staging

--- front-end/src/app/app.component.spec.ts
+++ front-end/src/app/app.component.spec.ts
@@ -25,14 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('front-end');
   });
-<<<<<<< HEAD
-
-  // it('should render title', () => {
-  //   const fixture = TestBed.createComponent(AppComponent);
-  //   fixture.detectChanges();
-  //   const compiled = fixture.nativeElement as HTMLElement;
-  //   expect(compiled.querySelector('.content span')?.textContent).toContain('front-end app is running!');
-  // });
-=======
->>>>>>> 93d51563defc6612aabc4ca9c7aa295656865c0b
 });
