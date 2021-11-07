ea7f6c894165eb246fb106e670da052511660778
hraose98 <harose98@revature.net>
2021-11-07 14:56:05 -0500

    buffer test

--- front-end/src/app/buffer/buffer.component.spec.ts
+++ front-end/src/app/buffer/buffer.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import {By} from '@angular/platform-browser';
 import { BufferComponent } from './buffer.component';
 
 describe('BufferComponent', () => {
@@ -21,5 +21,10 @@ describe('BufferComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have a buffer', () => {
+    const buttons = fixture.debugElement.queryAll(By.css('center'));
+    expect(buttons.length >= 1).toBeTruthy();
   });
 });
