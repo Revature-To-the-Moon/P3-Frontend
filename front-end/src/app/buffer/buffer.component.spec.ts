9c8db893407722b1add88f974566ac7211170888
hraose98 <harose98@revature.net>
2021-11-08 10:58:13 -0500

    buffer

--- front-end/src/app/buffer/buffer.component.spec.ts
+++ front-end/src/app/buffer/buffer.component.spec.ts
@@ -1,16 +1,35 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ActivatedRoute, RouterModule } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { AuthModule } from '@auth0/auth0-angular';
 import {By} from '@angular/platform-browser';
 import { BufferComponent } from './buffer.component';
 
 describe('BufferComponent', () => {
   let component: BufferComponent;
   let fixture: ComponentFixture<BufferComponent>;
+  let activatedRoute: ActivatedRoute = new ActivatedRoute;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ BufferComponent ]
+      declarations: [ BufferComponent ],
+      imports: [RouterModule, HttpClientTestingModule, RouterTestingModule, AuthModule.forRoot(
+        {
+          domain: 'dev-0w--5cqa.us.auth0.com',
+          clientId: '4LqYhiuu6amu7r3BOQH38phFDBycgDQB'
+        }
+        )],
+
+      providers: [
+        {
+          provide: ActivatedRoute,
+          useValue: activatedRoute
+        }
+      ]
     })
     .compileComponents();
+
   });
 
   beforeEach(() => {
@@ -24,7 +43,7 @@ describe('BufferComponent', () => {
   });
 
   it('should have a buffer', () => {
-    const buttons = fixture.debugElement.queryAll(By.css('center'));
+    const buttons = fixture.debugElement.queryAll(By.css('.buffer'));
     expect(buttons.length >= 1).toBeTruthy();
   });
 });
