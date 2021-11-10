9b169357cb21700c14e8371b1308e6281b036653
Hung Phan <htp217@gmail.com>
2021-11-10 12:29:00 -0500

    save work while pulling

--- front-end/src/app/profile/follow-button/follow-button.component.ts
+++ front-end/src/app/profile/follow-button/follow-button.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { ProfileService } from 'src/app/service/profile.service';
+import { Followings } from 'src/app/models/Followings';
+import { isTemplateSpan } from 'typescript';
 
 @Component({
   selector: 'app-follow-button',
@@ -8,6 +10,9 @@ import { ProfileService } from 'src/app/service/profile.service';
 })
 export class FollowButtonComponent implements OnInit {
   @Input() isFollow = false;
+  following!: Followings[];
+  followingUserName: string;
+
 
   constructor(public profileService: ProfileService) { }
 
@@ -18,4 +23,14 @@ export class FollowButtonComponent implements OnInit {
     this.isFollow = !this.isFollow;
   }
 
+  followingStatus(following: Followings[], followingUserName: string): void {
+    let j = 0;
+    for (let i = 0; i <= following.length; i++){
+      const follow = following[i];
+      if(follow.followingUserName !== followingUserName){
+        following[j] = follow;
+        j++;
+      }
+    }
+  }
 }
