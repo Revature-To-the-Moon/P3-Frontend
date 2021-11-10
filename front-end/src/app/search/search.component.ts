1827043e4497172b66435caa949d934197075517
Garrett Huon <ghuon407@gmail.com>
2021-11-10 15:53:07 -0500

    tryin search again

--- front-end/src/app/search/search.component.ts
+++ front-end/src/app/search/search.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Root } from '../models/root';
+import { RootServiceService } from '../service/root-service.service';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +9,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private service: RootServiceService) { }
+  roots:Root[] = []
   ngOnInit(): void {
+    this.service.getRoots().subscribe
+    (
+      data =>
+      {
+        this.roots = data;
+      }
+    )
   }
 
 }
--- front-end/src/app/service/root-service.service.ts
+++ front-end/src/app/service/root-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Root } from '../models/root';
 import { Comment } from '../models/Comment';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +28,10 @@ export class RootServiceService {
 
   getRootById(id: number): Promise<Root> {
     return this.http.get<Root>(this.rootUrl + '/' + id).toPromise();
+  }
+
+  getRoots(): Observable<any>{
+    return this.http.get<any>(this.rootUrl);
   }
 
 }
