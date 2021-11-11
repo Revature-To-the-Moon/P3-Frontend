628c91898c3cb94e5549414962f348b73d3696c0
Garrett Huon <ghuon407@gmail.com>
2021-11-11 17:22:24 -0500

    search bar tweaks

--- front-end/src/app/search/search.component.ts
+++ front-end/src/app/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ViewChild } from '@angular/core';
 import { Root } from '../models/root';
 import { RootServiceService } from '../service/root-service.service';
 
@@ -11,21 +11,33 @@ export class SearchComponent implements OnInit {
   searchText: string = "";
   value = '';
   constructor(private service: RootServiceService) { }
-  roots:Root[] = [];
+  roots: Root[] = [];
   result;
+  @ViewChild('box') inputBox;
 
-  onEnter(value: string) { this.value = value; 
-  this.result = this.roots.filter(res => res.title.toLocaleLowerCase().includes(this.value.toLocaleLowerCase()) || 
-  res.userName.toLocaleLowerCase().includes(this.value.toLocaleLowerCase()))
+  onEnter(value: string) {
+    this.value = value;
+    if (value) {
+      this.result = this.roots.filter(res => res.title.toLocaleLowerCase().includes(this.value.toLocaleLowerCase()) ||
+        res.userName.toLocaleLowerCase().includes(this.value.toLocaleLowerCase()))
+      console.log(this.result)
+    }
+    else if(!value){
+      this.result = [];
+      this.inputBox.nativeElement.value = '';
+    }
+  }
+  onClear(){
+    this.result = [];
   }
 
 
   ngOnInit(): void {
-    this.service.getAllRoots().then(result =>{
-      this.roots =result;
+    this.service.getAllRoots().then(result => {
+      this.roots = result;
     })
   }
-  
-  
+
+
 
 }
