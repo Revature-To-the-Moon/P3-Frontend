856612745cc7e55831827c8246f00a90204ae1c6
PeterclaverKimuli <30950839+PeterclaverKimuli@users.noreply.github.com>
2021-11-11 19:07:51 -0500

    liking and disliking commments

--- front-end/src/app/root/root.component.ts
+++ front-end/src/app/root/root.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Root } from '../models/root';
 import { RootServiceService } from '../service/root-service.service';
+import { AuthService } from '@auth0/auth0-angular';
+import { Vote } from '../models/vote';
 
 @Component({
   selector: 'app-root',
@@ -11,15 +13,28 @@ import { RootServiceService } from '../service/root-service.service';
 
 export class RootComponent implements OnInit {
 
-  constructor(private router: Router, private rootService: RootServiceService) { }
+  constructor(private router: Router, private rootService: RootServiceService, public auth: AuthService) { }
 
   roots: Root[] = [];
+  votes: Vote[] = [];
+
+  vote: Vote = {
+    id: 0,
+    userName: '',
+    value: 0,
+    commentId: 0
+  }
+  counter: number = 0;
 
   ngOnInit(): void {
     this.rootService.getAllRoots().then(result => {
       result.sort((a, b) => (a.dateTime < b.dateTime) ? 1 : -1);
       this.roots = result;
       console.log(result);
+    })
+
+    this.rootService.getAllVotes().then(result => {
+      this.votes = result;
     })
   }
 
@@ -49,6 +64,34 @@ export class RootComponent implements OnInit {
     this.rootService.getAllRoots().then(result => {
       result.sort((a, b) => (a.dateTime > b.dateTime) ? 1 : -1);
       this.roots = result;
+    })
+  }
+
+  likePost(id: number){
+    this.rootService.getRootById(id).then(result => {
+      result.totalVote++;
+
+      this.rootService.updateRoot(result).then(res => {
+        this.rootService.getAllRoots().then(result => {
+          result.sort((a, b) => (a.dateTime < b.dateTime) ? 1 : -1);
+          this.roots = result;
+        })
+        console.log("Post liked")
+      })
+    })
+  }
+
+  unLikePost(id: number){
+    this.rootService.getRootById(id).then(result => {
+      result.totalVote--;
+
+      this.rootService.updateRoot(result).then(res => {
+        this.rootService.getAllRoots().then(result => {
+          result.sort((a, b) => (a.dateTime < b.dateTime) ? 1 : -1);
+          this.roots = result;
+        })
+        console.log("Post unliked")
+      })
     })
   }
 }
--- front-end/src/app/service/root-service.service.ts
+++ front-end/src/app/service/root-service.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Root } from '../models/root';
 import { Comment } from '../models/Comment';
+import { Vote } from '../models/vote';
 
 @Injectable({
   providedIn: 'root'
@@ -10,6 +11,7 @@ export class RootServiceService {
 
   private rootUrl: string = "https://52.141.211.229/post/api/post";
   private rootUrl_1: string = "https://52.141.211.229/post/api/Comment";
+  private rootUrl_2: string = "https://52.141.211.229/post/api/Vote";
 
   constructor(private http: HttpClient) { }
 
@@ -17,8 +19,29 @@ export class RootServiceService {
     return this.http.post<Root>(this.rootUrl, root).toPromise();
   }
 
+  updateRoot(root: Root): Promise<Root> {
+    return this.http.put<Root>(this.rootUrl + "/" + root.id, root).toPromise();
+  }
+
   addComment(comment: Comment): Promise<Comment> {
     return this.http.post<Comment>(this.rootUrl_1, comment).toPromise();
+  }
+  getCommentById(id: number): Promise<Comment>{
+    return this.http.get<Comment>(this.rootUrl_1 + '/' + id).toPromise();
+  }
+  
+
+  getAllVotes(): Promise<Vote[]> {
+    return this.http.get<Vote[]>(this.rootUrl_2).toPromise();
+  }
+  addVote(vote: Vote): Promise<Vote>{
+    return this.http.post<Vote>(this.rootUrl_2, vote).toPromise();
+  }
+  updateVote(vote: Vote): Promise<Vote> {
+    return this.http.put<Vote>(this.rootUrl_2 + "/" + vote.id, vote).toPromise();
+  }
+  deleteVote(id: number): Promise<void> {
+    return this.http.delete<void>(this.rootUrl_2 + "/" + id).toPromise();
   }
 
   getAllRoots(): Promise<Root[]> {
