89daf92a8ffee10ec99f59092b5970bd0398f64f
nick-yelle <49138282+nick-yelle@users.noreply.github.com>
2021-11-12 10:34:37 -0500

    Update karma-ci.conf.js

--- front-end/karma-ci.conf.js
+++ front-end/karma-ci.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/sonarcloud'),
+      dir: require('path').join(__dirname, '.'),
       subdir: '.',
       reporters: [
         { type: 'lcovonly' },
