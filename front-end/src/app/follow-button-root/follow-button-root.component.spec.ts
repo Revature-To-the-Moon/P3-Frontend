4736f886063643a2e3edf9898aeb146f5f2f477c
Larry Moua <lkmoua@gmail.com>
2021-11-15 16:00:32 -0500

    fixed tests and pulled from main

--- front-end/src/app/follow-button-root/follow-button-root.component.spec.ts
+++ front-end/src/app/follow-button-root/follow-button-root.component.spec.ts
@@ -1,6 +1,7 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { FollowButtonRootComponent } from './follow-button-root.component';
+import { AuthModule } from '@auth0/auth0-angular';
 
 describe('FollowButtonRootComponent', () => {
   let component: FollowButtonRootComponent;
@@ -8,7 +9,12 @@ describe('FollowButtonRootComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FollowButtonRootComponent ]
+      declarations: [ FollowButtonRootComponent ],
+      imports: [ HttpClientTestingModule, AuthModule.forRoot(
+        {
+          domain: 'dev-0w--5cqa.us.auth0.com',
+          clientId: '4LqYhiuu6amu7r3BOQH38phFDBycgDQB'
+        })]
     })
     .compileComponents();
   });
